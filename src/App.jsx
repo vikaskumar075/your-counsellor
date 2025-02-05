4573bdf971cb5835164ef22d56a8748667648ef0
pankaj upadhyay <pankajup.git01@gmail.com>
2025-02-05 12:46:28 +0530

    testimonial added

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
 import Hero from "./Component/Hero";
 import Navbar from "./Component/Navbar";
+import { AnimatedTestimonials } from "./Component/ui/animated-testimonials";
+import { testimonials } from "./data/testimonial";
 
 
 const App = () => {
   return (
     <div>
       <Navbar />
-   
-      <section id="home" className="h-screen flex justify-center items-center bg-gray-100">
-        <Hero/>
+
+      <section id="home" className="h-screen flex justify-center items-center ">
+        <Hero />
       </section>
-      <section id="about" className="h-screen flex justify-center items-center bg-blue-100">
-        <h1 className="text-4xl font-bold">About</h1>
+      <section id="about" className="h-screen flex justify-center items-center ">
       </section>
-      <section id="services" className="h-screen flex justify-center items-center bg-green-100">
+      <section id="services" className="h-screen flex justify-center items-center">
         <h1 className="text-4xl font-bold">Services</h1>
       </section>
-      <section id="courses" className="h-screen flex justify-center items-center bg-yellow-100">
+      <section id="courses" className="h-screen flex justify-center items-center  ">
         <h1 className="text-4xl font-bold">Courses</h1>
       </section>
-      <section id="testimonial" className="h-screen flex justify-center items-center bg-red-100">
-        <h1 className="text-4xl font-bold">Testimonial</h1>
+      <section id="testimonial" className="bg-red-300 h-auto ">
+        <AnimatedTestimonials testimonials={testimonials} autoplay={true} />
       </section>
-      <section id="contact" className="h-screen flex justify-center items-center bg-purple-100">
-        <h1 className="text-4xl font-bold">Contact</h1>
-      </section>
+
     </div>
   );
 };
